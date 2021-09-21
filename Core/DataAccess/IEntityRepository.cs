d55c4f9e9147455b52fe31f9d937fefb790dece0
beyto <beyto@BEYTULLAH>
2021-09-21 13:35:47 +0300

    Add project files.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    //Core katmanı diğer katmanları referans almaz. Ama diğer katmanlar Core katmanını referans alırlar.
+    // class : referans tip
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() : new'lenebilir olmalı.
+    public interface IEntityRepository<T> where T : class, IEntity,new() //T 'yi where ile sınırlandırabiliyoruz.(T referans tip olsun ve IEntity veya IEntity'den implemente edilen bir nesne olabilir.New lenmelidir ayrıca.Yani interfaceleri kullanamayız.) 
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); //Filtre vermessek null'dür yani bir filtre vermessek tümünü getirir.
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+
+        public Result(bool success, string message) : this(success) // İki parametre gönderen birisi için 2 parametreli constructor çalışır. Aynı zamanda "this" ilede bir parametreli olanıda çalıştır demiş oldum.
+        {
+            Message = message; // Altta tanımlamış olduğumuz Message sadece get yani okunabilir olması rağmen constructor içerisinde set edilebilir !
+        }
+
+        public Result(bool success)
+        {   
+            Success = success;  // Altta tanımlamış olduğumuz Message sadece get yani okunabilir olması rağmen constructor içerisinde set edilebilir !
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
